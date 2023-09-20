50a9de67cd223d2ace09f58f6bac01e2a2c6ad13
bdina1 <brian.n.dina@gmail.com>
2023-09-20 15:17:24 -0400

    Add .gitignore for deep learning project

--- video_chatgpt/train/geo_gpt_train.py
+++ video_chatgpt/train/geo_gpt_train.py
@@ -24,9 +24,43 @@ DEFAULT_BOS_TOKEN = "</s>"
 DEFAULT_UNK_TOKEN = "<unk>"
 
 
+
 @dataclass
-class  ModelArguments:
-    model_name_or_path: Optional[str] = feild(default="facebook/opt-125")
+class ModelArguments:
+    model_name_or_path: Optional[str] = field(default="facebook/opt-125m")
+    version: Optional[str] = field(default="v0")
+    freeze_backbone: bool = field(default=False)
+    tune_mm_mlp_adapter: bool = field(default=False)
+    pretrain_mm_mlp_adapter: Optional[str] = field(default=None)
+    mm_use_vid_start_end: bool = field(default=False)
+
+
+@dataclass
+class DataArguments:
+    data_path: str = field(default=None,
+                           metadata={"help": "Path to the training data."})
+    lazy_preprocess: bool = False
+    is_multimodal: bool = False
+    sep_video_conv_front: bool = False
+    video_token_len: int = 0
+    video_folder: Optional[str] = field(default=None)
+    frame_aspect_ratio: str = 'square'
+
+
+@dataclass
+class TrainingArguments(transformers.TrainingArguments):
+    cache_dir: Optional[str] = field(default=None)
+    optim: str = field(default="adamw_torch")
+    remove_unused_columns: bool = field(default=False)
+    freeze_mm_mlp_adapter: bool = field(default=False)
+    force_fsdp: bool = field(default=False)
+    model_max_length: int = field(
+        default=512,
+        metadata={
+            "help":
+                "Maximum sequence length. Sequences will be right padded (and possibly truncated)."
+        },
+    )
 
 def train():
     parser = transformers.HfArgumentParser(
